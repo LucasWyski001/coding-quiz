5357144447bf361dee4a03448a048ef8672e35ea
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-07-07 17:37:29 -0500

    added a second JS file, as well as a css and updated code

--- script.js
+++ script.js
@@ -1,122 +1,129 @@
+const timerElement = document.getElementById("timer");
+const leaderboards = [];
 const quizQuestions = [
-    {
-        question: "Question 1",
-        choices :["Option A", "Option B", "Option C", "Option D"],
-        answer: 3
-    },
-    {
-        question: "Question 2",
-        choices : ["Option A", "Option B", "Option C", "Option D"],
-        answer: 2
-    },
-    {
-        question: "Question 3",
-        choices : ["Option A", "Option B", "Option C", "Option D"],
-        answer: 0
-    },
-    {
-        question: "Question 4",
-        choices : ["Option A", "Option B", "Option C", "Option D"],
-        answer: 1
-    }
-]
-
-// variables for the test
-let currentQuestion = 0;
-let time = 60;
-let timerInterval;
+{
+  question: "question 1",
+  options: ["Right","Wrong","Wrong","Wrong"],
+  answer: 0,
+},
+{
+  question: "question 2",
+  options: ["Wrong","Right","Wrong","Wrong"],
+  answer: 1,
+},
+{
+  question: "question 3",
+  options: ["Wrong","Wrong","Wrong","Right"],
+  answer: 3,
+},
+{
+  question: "question 4",
+  options: ["Wrong","Wrong","Right","Wrong"],
+  answer: 2,
+},
+];
+let currentQuestionIndex = 0;
 let score = 0;
-
-const startButton = document.getElementById("start");
+let timeLeft = 100;
+let timerInterval;
+var endTime;
+const startElement = document.getElementById("start-button");
 const questionElement = document.getElementById("question");
-const optionsElement = document.getElementById("multipleChoice");
-const submitButton = document.getElementById("submit-btn");
-const initialsInput = document.getElementById("initials");
-
-// This event listener will start the quiz
-startButton.addEventListener("click", startQuiz);
-// This function is what actually makes the quiz application run
-function startQuiz(){
-    startButton.style.display = "none";
-    timerInterval = setInterval(updateTimer, 1000);
-    displayQuestion();
-}
-
-// This function will make the questions work 
-function displayQuestion() {
-    const currentQuizData = quizQuestions[currentQuestion];
-    questionElement.innerText = currentQuizData.question;
-    optionsElement.innerHTML = "";
-
-    for (let i = 0; i < currentQuizData.choices.length; i++) {
-        const option = document.createElement("li");
-        option.textContent = String.fromCharCode(65 + i) + ". " + currentQuizData.choices[i];
-        option.addEventListener("click", () => checkAnswer(i));
-        optionsElement.appendChild(option);
-    }
-}
-
-// This loop goes through the answers to check wether the answers are right or wrong
-// if the answer is wrong it will deduct 10 seconds from the user (thus giving them more stress to finish)
-function checkAnswer(answerIndex){
-    const currentQuizData = quizQuestions[currentQuestion];
-
-    if (answerIndex === currentQuizData.answer) {
-        if (currentQuestion < quizQuestions.length -1){
-            currentQuestion++;
-            displayQuestion();
-        } else{
-            endQuiz();
-        }
-    } else{
-        time -= 10;
-    }
-    saveScore();
-}
+const optionsElement = document.getElementById("options");
+const submitElement = document.getElementById("submit-button");
+const textboxElement = document.getElementById("initials");
 
 
 
-function updateTimer(){
-    time--;
-    if (time <= 0){
-        endQuiz();
-    }
-    timer.textContent = time;
-}
+startElement.addEventListener("click", startButtonClicked);
+submitElement.addEventListener("click", saveScore);
+submitElement.style.display = "none";
+textboxElement.style.display = "none";
 
 
-// this function will end the quiz of the application
+
+
+function startButtonClicked() {
+  setQuestion();
+  startElement.style.display = "none";
+  timerInterval = setInterval(updateTimer, 1000);
+  displayHighscore();
+};
+
+
+function updateTimer() {
+  timeLeft--;
+  timerElement.textContent = timeLeft;
+  if (timeLeft <= 0) {
+    endQuiz();
+  }
+};
+
+
+function setQuestion() {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+  optionsElement.innerHTML = "";
+  for (let i = 0; i < currentQuestion.options.length; i++) {
+    const choice = document.createElement("li");
+    choice.textContent = currentQuestion.options[i];
+    choice.addEventListener("click", () => {
+      checkAnswer(i);
+    });
+    optionsElement.appendChild(choice);
+  };
+};
+function checkAnswer(answerIndex) {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+  if (timeLeft <= 0){
+    endQuiz();
+  };
+  if (answerIndex === currentQuestion.answer) {
+    score++;
+  } else {
+    timeLeft -= 25;
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < quizQuestions.length) {
+    setQuestion();
+  } else {
+      endQuiz();
+  }
+  saveScore();
+};
+
+
 function endQuiz() {
-    clearInterval(timerInterval);
-    timer.style.display = "none";
-    questionElement.innerText = "Completed Quiz!";
-    optionsElement.innerHTML = "";
-    initialsInput.style.display = "block";
-    submitButton.style.display = "block";
-}
+  clearInterval(timerInterval);
+  endTime = timeLeft;
+  timerElement.style.display = "none";
+  optionsElement.style.display = "none";
+  textboxElement.style.display = "block";
+  submitElement.style.display = "block";
+  questionElement.textContent = "Your score is " + score + " out of 100, with " + endTime + " seconds left. Enter your initials and click submit to save your score!";
+  localStorage.setItem("leaderboards", score.toString());
+};
 
+
+submitElement.addEventListener("click", saveScore);
 function displayHighscore() {
-    const storedHighScore = localStorage.getItem("HighScore");
-    if (storedHighScore){
-        highScore = parseInt(storedHighScore);
-    }
-
-    const highScoreElement = document.getElementById("high-score");
-    highScoreElement.textContent = highScore.toString();
+  const storedHighScore = localStorage.getItem("highScore");
+  if (storedHighScore) {
+    score = parseInt(storedHighScore);
+  }
+  const highScoreElement = document.getElementById("high-score");
+  highScoreElement.textContent = score.toString();
 }
 
 
-
-// this section will save score and take initials
-submitButton.addEventListener("click", saveScore);
-
-function saveScore(){
-    const initials = initialsInput.value;
-    console.log("Initials:", initials);
-    console.log("Score:", time);
-
-    if (time > highscore){
-        highscore = time;
-        console.log("New high score:", highscore)
-    }
-}
+function saveScore() {
+  var initials = textboxElement.value;
+  leaderboards.push(["user: " + initials, "score: " + score, "time left: " + endTime]);
+  console.log(leaderboards);
+  console.log('Score:', timeLeft)
+  if (timeLeft > score) {
+    score = timeLeft;
+    console.log("New High Score: ", score);
+  }
+  localStorage.setItem("leaderboards", JSON.stringify(leaderboards));
+};
